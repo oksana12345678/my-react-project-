5432cf4fc6071d4aafcab3c916c5730443802c5d
oksana12345678 <oksana.tanashuk@gmail.com>
2024-03-19 16:59:36 +0100

    add-fix

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import Product from "./Product";
+export default function App() {
+  return (
+    <div>
+      <h1>Products</h1>
+
+      <Product />
+      <Product />
+      <Product />
+    </div>
+  );
+}
