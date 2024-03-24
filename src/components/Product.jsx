3994b452bdea9d157caf066c571df24245865f8c
oksana12345678 <oksana.tanashuk@gmail.com>
2024-03-24 12:50:23 +0100

    add

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,16 +1,23 @@
-export const Product = () => {
-  const price = 999;
+// src/components/Product.jsx
 
+// export const Product = (props) => {
+//   return (
+//     <div>
+//       <h2>{props.name}</h2>
+//       <img src={props.imgUrl} alt={props.name} width="480" />
+//       <p>Price: {props.price} credits</p>
+//     </div>
+//   );
+// };
+
+export const Product = ({ name, imgUrl, price }) => {
   return (
-    <>
-      <h2>Tacos</h2>
-      <img
-        src="<https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640>"
-        alt="Tacos With Lime"
-        width="640"
-      />
+    <div>
+      <h2>{name}</h2>
+      <img src={imgUrl} alt={name} width="480" />
       <p>Price: {price} credits</p>
-    </>
+    </div>
   );
 };
+
 export default Product;
