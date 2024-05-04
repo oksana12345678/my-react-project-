617521402630b2324924a6c7280b00b13441442c
Oksana Tanashchuk <oksana.tanashuk@gmail.com>
2024-05-04 10:01:10 +0200

    add

--- src/components/ArticlesList/ArticlesList.jsx
+++ src/components/ArticlesList/ArticlesList.jsx
@@ -2,10 +2,14 @@ const ArticlesList = ({ items }) => {
   return (
     <div>
       <ul>
-        {items.map(({ objectID, url, title }) => (
+        {items.map(({ objectID, url, title, story_title, story_url }) => (
           <li key={objectID}>
-            <a href={url} target="_blank" rel="noreferrer noopener">
-              {title}
+            <a
+              href={url ? url : story_url}
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              {title ? title : story_title ? story_title : "No title available"}
             </a>
           </li>
         ))}
